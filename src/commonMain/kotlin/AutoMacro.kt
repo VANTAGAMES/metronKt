addf30c61715240529ee3bfdd425682efb833f90
Bruce0203 <BruceFreedy@gmail.com>
2023-07-14 14:42:53 +0900

    테스트: 매크로 코드 커밋

---
+++ src/commonMain/kotlin/AutoMacro.kt
@@ -0,0 +1,16 @@
+import korlibs.korge.view.*
+import korlibs.time.*
+
+fun State.autoMacro() {
+    container.onEvent(UpdateEvent) {
+        if (isPaused) return@onEvent
+        println("asdf")
+        if (note.stickAngle.elapsed <= 0.seconds) {
+            return@onEvent
+        }
+        println(note.stickAngle.elapsed - note.curr.seconds*bpmToSec)
+        if (note.stickAngle.elapsed >= note.curr.seconds) {
+            audit(delta = 0.seconds)
+        }
+    }
+}
