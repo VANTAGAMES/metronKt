27979bb41a580b2b7c1a8c2a09914c828bdcc3b7
Bruce0203 <BruceFreedy@gmail.com>
2023-07-31 15:12:16 +0900

    구현: 자동 플레이 기능 추가, 키보드 P키로 토클 가능

--- src/commonMain/kotlin/AutoMacro.kt
+++ src/commonMain/kotlin/AutoMacro.kt
@@ -1,12 +1,25 @@
+import effect.*
+import korlibs.event.*
+import korlibs.korge.input.*
 import korlibs.korge.view.*
+import korlibs.korge.view.align.*
 import korlibs.math.geom.*
 import korlibs.math.geom.abs
 import korlibs.time.*
 import kotlin.math.*
 
 fun State.autoMacro() {
+    val txt = txtWithFilter("자동 플레이") {}.alignX(sceneContainer, 2.0, true)
+//        .visible(false)
+    container.keys {
+        down(Key.P) {
+            txt.visible = !txt.visible
+        }
+
+    }
     container.onEvent(UpdateEvent) {
         if (isPaused) return@onEvent
+        if (!txt.visible) return@onEvent
 //        println("asdf")
         if (note.stickAngle.elapsed <= 0.seconds) {
             return@onEvent
