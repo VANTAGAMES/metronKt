025927ef56fb39ca63813d781e719dd55207b289
Bruce0203 <BruceFreedy@gmail.com>
2023-09-22 14:48:02 +0900

    아래쪽 베이스 z 인덱스 올림

--- client/src/commonMain/kotlin/metron/app/components/PlayerStick.kt
+++ client/src/commonMain/kotlin/metron/app/components/PlayerStick.kt
@@ -1,11 +1,13 @@
 package metron.app.components
 
 import com.github.quillraven.fleks.*
+import korlibs.datastructure.iterators.*
 import korlibs.image.color.*
 import korlibs.korge.view.*
 import korlibs.korge.view.align.*
 import korlibs.math.geom.*
 import korlibs.math.geom.shape.*
+import korlibs.memory.*
 import metron.*
 import metron.app.Stage
 import metron.util.*
@@ -21,42 +23,46 @@ class PlayerStick(val stage: Stage) : Component<PlayerStick> {
         val baseBodyColor = Colors["818589"]
         val upperBaseBodyColor = Colors["D3D3D3"]
         override val onAdded: ComponentHook<PlayerStick> = { entity, playerStick ->
-            screen.container {
-                val baseUpperWidth = 225f
-                val baseWidth = 400f
-                val roofHeight = -(stickHeight/20)
-                shapeView(pointArrayListOf(
-                    Point(0, -roofHeight),
-                    Point(0, 0),
-                    Point(baseUpperWidth/2, roofHeight),
-                    Point(baseUpperWidth, 0),
-                    Point(baseUpperWidth, -roofHeight),
-                ).toPolygon()) {
-                }.colorMul(upperBaseBodyColor)
-                shapeView(pointArrayListOf(
-                    Point(0, 0),
-                    Point(baseUpperWidth, 0),
-                    Point(baseWidth, stickHeight),
-                    Point(-baseWidth + baseUpperWidth, stickHeight)
-                ).toPolygon()).colorMul(baseBodyColor)
-                val ratio = 1/5f
-                val bottomBaseHeight = stickHeight*ratio
-                val side = sqrt(((baseWidth - baseUpperWidth) / 2f).pow(2) + stickHeight.pow(2))
-                val outline = sqrt((side * ratio).pow(2) - bottomBaseHeight.pow(2))
-                val a = baseWidth - outline *2f
-                println(ratio)
-                shapeView(pointArrayListOf(
-                    Point(-outline*8f, 0),
-                    Point(a, 0),
-                    Point(baseWidth, bottomBaseHeight),
-                    Point(baseUpperWidth - baseWidth, bottomBaseHeight)
-                ).toPolygon()) {
-                    positionY(stickHeight - height)
-                }.colorMul(upperBaseBodyColor)
-                centerXOn(screen)
-                positionY((screen.height - stickY)*2 + stickWidth/2)
+            val baseUpperWidth = 225f
+            val baseWidth = 400f
+            val roofHeight = -(stickHeight/20)
+            val ratio = 1/6f
+            val bottomBaseHeight = stickHeight*ratio
+            val side = sqrt(((baseWidth - baseUpperWidth) / 2f).pow(2) + stickHeight.pow(2))
+            val outline = sqrt((side * ratio).pow(2) - bottomBaseHeight.pow(2))
+            val bottomBaseWidth = baseWidth - outline *2f
+            arrayOf(
+                screen.container {
+                    shapeView(pointArrayListOf(
+                        Point(0, -roofHeight),
+                        Point(0, 0),
+                        Point(baseUpperWidth/2, roofHeight),
+                        Point(baseUpperWidth, 0),
+                        Point(baseUpperWidth, -roofHeight),
+                    ).toPolygon()) {
+                    }.colorMul(upperBaseBodyColor)
+                    shapeView(pointArrayListOf(
+                        Point(0, 0),
+                        Point(baseUpperWidth, 0),
+                        Point(baseWidth, stickHeight),
+                        Point(-baseWidth + baseUpperWidth, stickHeight)
+                    ).toPolygon()).colorMul(baseBodyColor)
+                }.zIndex(-10),
+                screen.container {
+                    shapeView(pointArrayListOf(
+                        Point(-outline*(7.nextPowerOfTwo), 0),
+                        Point(bottomBaseWidth, 0),
+                        Point(baseWidth, bottomBaseHeight),
+                        Point(baseUpperWidth - baseWidth, bottomBaseHeight)
+                    ).toPolygon()) {
+                        positionY(stickHeight - height)
+                    }.colorMul(upperBaseBodyColor)
+                }.zIndex(10)
+            ).fastForEach {
+                it.centerXOn(screen)
+                it.positionY((screen.height - stickY) * 2 + stickWidth / 2)
+            }
 
-            }.zIndex(-10)
             playerStick.body = screen.container().apply {
                 val stick = fastRoundRect(
                     corners = RectCorners(1),
