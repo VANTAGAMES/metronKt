fb85a8801672ce330a19d3c1a4d53bee1a638255
Bruce0203 <BruceFreedy@gmail.com>
2023-09-28 12:56:59 +0900

    모바일 햅틱 진동 효과

--- client/src/commonMain/kotlin/metron/app/handlers/HitEffect.kt
+++ client/src/commonMain/kotlin/metron/app/handlers/HitEffect.kt
@@ -1,9 +1,11 @@
 package metron.app.handlers
 
 import event.*
+import korlibs.korge.service.haptic.*
 import korlibs.korge.view.*
 import korlibs.korge.view.align.*
 import korlibs.math.interpolation.*
+import korlibs.render.*
 import korlibs.time.*
 import metron.*
 import metron.app.Stage
@@ -13,6 +15,7 @@ import metron.util.Effect.Companion.easingEffect
 fun Stage.enableHitEffect() {
     val dummyView = screen.dummyView()
     screen.onEvent(HitEvent) {
+        scene.views.hapticFeedback.emit(GameWindow.HapticFeedbackKind.GENERIC)
         if (isStopped || isStickPaused) return@onEvent
         dummyView.easingEffect(0.05.seconds, Easing.LINEAR, arrayOf(
             Effect { _, value -> camera.scale(1 + (1 - value) * 0.01); camera.centerOn(screen) }
